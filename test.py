5d119506e62d545d7b8c30cf42064fa349c9ce2a
bernarde01 <bernard.esterhuyse@gmail.com>
2022-02-10 11:56:32 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import time
+
+# print("Hello World!")
+
+print("App started")
+run = True
+counter = 0
+while run == True:
+    print("Count: " + str(counter))
+    print("System time: " + str(dt.datetime.now()))
+    time.sleep(5)
+    counter+=1
+
+    # currenttime = dt.datetime.now()
+    # if currenttime.second == 0 and currenttime.second % 10 == 0:
+    #     print(currenttime)
+    #     print("Hello World2!")
